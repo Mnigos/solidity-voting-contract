3da4730b57301a6c317eb54b6b72ad7622cad5da
Mnigos <makowskiigor@gmail.com>
2022-09-07 20:55:54 +0200

    fix(typechain): generating types

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import '@nomiclabs/hardhat-ethers'
 import 'hardhat-watcher'
 import 'solidity-coverage'
 import 'dotenv/config'
+import '@typechain/hardhat'
 
 task('accounts', 'Prints the list of accounts', async (taskArguments, hre) => {
   const accounts = await hre.ethers.getSigners()
@@ -29,6 +30,10 @@ const configuration: HardhatUserConfig = {
       url: `https://goerli.infura.io/v3/${process.env.API_KEY}`,
       accounts: [process.env.PRIVATE_KEY ?? ''],
     },
+  },
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v5',
   },
 }
 export default configuration
