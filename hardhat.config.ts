33b5e20689c03c03451ff5457ae186b9b44e6400
Mnigos <makowskiigor@gmail.com>
2022-09-07 20:45:15 +0200

    chore(hardhat.config): add networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig, task } from 'hardhat/config'
 import '@nomiclabs/hardhat-ethers'
 import 'hardhat-watcher'
 import 'solidity-coverage'
+import 'dotenv/config'
 
 task('accounts', 'Prints the list of accounts', async (taskArguments, hre) => {
   const accounts = await hre.ethers.getSigners()
@@ -19,6 +20,14 @@ const configuration: HardhatUserConfig = {
       files: ['./test/**/*'],
       verbose: true,
       clearOnStart: true,
+    },
+  },
+  defaultNetwork: 'goerli',
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY ?? ''],
     },
   },
 }
